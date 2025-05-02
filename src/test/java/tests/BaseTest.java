456ec97d3c07e15fbb2fc5ac94bc393195fddb37
Sergey Barykov <greyfoxsocks@yandex.com>
2025-05-02 21:58:30 +0300

    Added BaseTest.java, changed main files, deleted App.java and AppTest.java

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
